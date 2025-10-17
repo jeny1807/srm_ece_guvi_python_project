282bc6f397f2208e91f4b3c429a4a5ff1e1c22b5
jeny1807 <hi.jenifer07@gmail.com>
2025-10-17 12:45:49 +0530

    Add files via upload

---
+++ PROGRAM2.py
@@ -0,0 +1,16 @@
+#calculate the total electricity bill based on units consumed upt
+unit=int(input("enter the units consumed"))
+if(unit<=100):
+    amount1=unit*5
+    print(amount1)
+elif(unit<=200):
+    amount2=unit*7
+    print(amount2)
+else:
+    amount3=unit*10
+    print(amount3)
+    
+
+
+
+
